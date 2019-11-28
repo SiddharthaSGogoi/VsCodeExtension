c4e4f3cb5dd9ef64985b4498be9d24accc4acc28
SiddharthaSGogoi <siddhartha.gogoi@kayadev.com>
2019-11-28 15:23:37 +0530

    Added extension to listen on startup.

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,10 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('extension.testName', () => {
 		vscode.window.showInformationMessage('Hello World!');
 		vscode.window.showErrorMessage('Heyyyy heyy heyy');
-	});
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
