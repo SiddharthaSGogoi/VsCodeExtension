ffc93d33dff5b3f9fc65e19dd9493e09d012f67f
SiddharthaSGogoi <siddhartha.gogoi@kayadev.com>
2019-11-28 15:37:32 +0530

    Reverted back to this.

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+
 		vscode.window.showInformationMessage('Hello World!');
 		vscode.window.showErrorMessage('Heyyyy heyy heyy');
-
-	// context.subscriptions.push(disposable);
+	});
+	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
