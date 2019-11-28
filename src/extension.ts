4906843f70f1146dc2388400604a26298ff444e3
SiddharthaSGogoi <siddhartha.gogoi@kayadev.com>
2019-11-28 11:57:02 +0530

    Changed version of vscode.

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.window.showInformationMessage('Hello World!');
-	vscode.window.showErrorMessage('Heyyyy heyy heyy')
-	// let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.testName', () => {
+		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showErrorMessage('Heyyyy heyy heyy');
+	});
 
-	// });
-
-	// context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
